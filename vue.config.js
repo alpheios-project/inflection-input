2382a489d88ad2d2200fdf9eca70ca9e8ae99347
Irina <irina060981@yandex.ru>
2020-02-05 22:54:45 +1000

    small fixes

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src/')
+      }
+    }
+  },
+  chainWebpack: config => {
+    config
+      .module
+      .rule('csv')
+      .test(/\.csv$/)
+      .use('csv-loader')
+      .loader('csv-loader')
+      .options({
+        dynamicTyping: true,
+        header: true,
+        skipEmptyLines: true
+      })
+      .end()
+  }
+}
