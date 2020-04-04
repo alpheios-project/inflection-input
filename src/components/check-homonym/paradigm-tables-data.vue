2831f95cb95d59e5bdffbcfae3f48885914d0892
Irina <irina060981@yandex.ru>
2020-04-04 23:01:36 +1000

    add subtables

--- src/components/check-homonym/paradigm-tables-data.vue
+++ src/components/check-homonym/paradigm-tables-data.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="paradigm-tables-data" v-if="views && views.length > 0">
-        <div class="paradigm-tables-data-item" v-for="(view, viewID) of views" v-bind:key="viewID" >
+        <div class="paradigm-tables-data-item" v-for="(view, viewId) of views" v-bind:key="viewId" >
             <paradigm-table :table="view.wideTable" :title="getTitle(view)" @saveFeedback="saveFeedback"/>
+
+            <div v-for="(table, tableId) in view.wideSubTables" v-bind:key="tableId">
+              <paradigm-table :table="table" @saveFeedback="saveFeedback"/>
+            </div>
         </div>
     </div>
 </template>
